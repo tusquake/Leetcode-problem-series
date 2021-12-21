f16b474c51bd43b6f813102a3dc7bea33e517c8d
tusquake <77339749+tusquake@users.noreply.github.com>
2021-12-21 09:36:58 +0530

    Update Two_sum.cpp

--- Two_sum.cpp
+++ Two_sum.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> v;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if((arr[i]+arr[j])==target){
+        for(int i=0;i<nums.size()+1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if((nums[i]+nums[j])==target){
                     v.push_back(i);
                     v.push_back(j);
+                    break;
+                    sort(v.begin(),v.end());
                 }
+                
+                
             }
         }
         return v;
     }
-
 };
-
-int main(){
-    Solution s;
-    int t;
-    cin>>t;
-    vector<int> v;
-    s.twoSum(v,t);
-}
